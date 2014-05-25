5a218754d3899f9fa019f314c15f2f0862fd188c
Ryan Brubaker <ryan.brubaker@gmail.com>
2014-05-24 21:19:13 -0500

    Final script to generate tidy data set

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,8 @@
+library(reshape2)
+
 DATA_FOLDER <- "./UCI HAR Dataset"
 NUM_TEST_OBS <- 2947
 NUM_TRAIN_OBS <- 7352
-
 
 # "main" method to create the tidy data set
 create_data_set <- function()
@@ -15,8 +16,9 @@ create_data_set <- function()
     # Read in general data
     activity_factors <- read.table(paste(DATA_FOLDER, "activity_labels.txt", sep="/"))
     features <- read.table(paste(DATA_FOLDER, "features.txt", sep="/"))    
-    filtered_features <- features[grepl("-mean", features[,2]) | grepl("-std", features[,2]), 2]
-    
+    filtered_features <- features[grepl("-mean\\(\\)", features[,2]) | grepl("-std\\(\\)", features[,2]),]    
+    filtered_features_char <- standardize_column_names(sapply(filtered_features[,2], function(x) as.character(x)))
+
     # Read in test data
     test_subjects <- scan(paste(DATA_FOLDER, "test", "subject_test.txt", sep="/"))
     test_activities <- read_activities_file("test", activity_factors)
@@ -25,8 +27,8 @@ create_data_set <- function()
                             colClasses="numeric", 
                             nrows=NUM_TEST_OBS)
     
-    filtered_test_obs <- test_obs[, filtered_features]
-    
+    filtered_test_obs <- test_obs[, filtered_features[,1]]
+        
     # Read in training data
     train_subjects <- scan(paste(DATA_FOLDER, "train", "subject_train.txt", sep="/"))
     train_activities <- read_activities_file("train", activity_factors)
@@ -35,14 +37,25 @@ create_data_set <- function()
                             colClasses="numeric", 
                             nrows=NUM_TRAIN_OBS)
 
-    filtered_train_obs <- train_obs[, filtered_features]
+    filtered_train_obs <- train_obs[, filtered_features[,1]]
 
     tidy_data_frame <- rbind(filtered_test_obs, filtered_train_obs)
     tidy_data_frame <- cbind(c(test_subjects, train_subjects), c(test_activities, train_activities), tidy_data_frame)
-    colnames(tidy_data_frame) <- c("subject", "activity", filtered_features)
+    colnames(tidy_data_frame) <- c("subject", "activity", filtered_features_char)
 
-    write.table(tidy_data_frame, file = "tidy_data.csv", sep = ",")
+    
+    tidy_data_frame_melted <- melt(tidy_data_frame, 
+                                   id=c("subject", "activity"),
+                                   measure.vars=filtered_features_char)
+    
+    tidy_data_frame_casted <- dcast(tidy_data_frame_melted,
+                                    subject + activity ~ variable, mean)
+    
+    
+    write.csv(tidy_data_frame_casted, file = "tidy_data.csv", row.names=FALSE)
+    return(tidy_data_frame_casted)
 }
+
 
 # Checks for the presence of the data folder or the zipped version of
 # the data. If the zip file exists, we'll unzip it. If neither the data
@@ -71,6 +84,34 @@ read_activities_file <- function(base_dir, factors)
 {
     file_name = paste("y_", base_dir, ".txt", sep="")
     activity_data <- scan(paste(DATA_FOLDER, base_dir, file_name, sep="/"))
-    activity_data <- sapply(activity_data, function(x) factors[x, 2])
+    activity_data <- sapply(activity_data, function(x) as.character(factors[x, 2]))
+}
+
+
+# Standardize column names to conform to some of the suggestions
+# made in class
+standardize_column_names <- function(col_names)
+{
+    # replace starting t's with time
+    # replace starting f's with frequencey
+    col_names <- sub("^t", "time", col_names)
+    col_names <- sub("^f", "frequency", col_names)
+    
+    # remove "()" and "-"
+    col_names <- gsub("\\(\\)", "", col_names)
+    col_names <- gsub("-", "", col_names)
+    
+    # replace "mean" with "mean"
+    # replace "std" with "Std"
+    col_names <- gsub("mean", "Mean", col_names)
+    col_names <- gsub("std", "Std", col_names)
+    
+    # other substitutions
+    col_names <- gsub("Acc", "Acceleration", col_names)
+    col_names <- gsub("Freq", "Frequency", col_names)
+    col_names <- gsub("BodyBody", "Body", col_names)
+    col_names <- gsub("Mag", "Magnitude", col_names)
+    
+    return (col_names)
 }
     
